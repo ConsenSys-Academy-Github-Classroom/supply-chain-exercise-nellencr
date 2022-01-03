88431ffe20b734fe86ddaf7fe7b6c587f55bdb17
NELA <nellencr@gmail.com>
2022-01-03 13:03:34 +0200

    Update TestSupplyChain.sol

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,14 +5,43 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
+    SupplyChain myContract;
+    uint value = 1000;
+
+    function beforeEach () public {
+      myContract = new SupplyChain();
+      myContract.addItem('testItem', value);
+    }
 
     // Test for failing conditions in this contracts:
     // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
-
     // buyItem
 
+    function testBuyItem () public {
+      bool success;
+      string memory name;
+      uint sku;
+      uint price;
+      uint state;
+      address seller;
+      address buyer;
+
+      (name, sku, price, state, seller, buyer) = myContract.fetchItem(0);
+
+      Assert.equal(sku, 0, "Expected Sku");
+      Assert.equal(price, value, "Expected Price");
+      Assert.equal(name,'testItem', "Expected item name");
+
+      (success, ) = address(myContract).call.value(price + 1000)(
+        abi.encodeWithSignature("buyItem(uint sku)", sku)
+      );
+      // // FIXME: False positive
+      Assert.isFalse(success, "Not enough eth");
     // test for failure if user does not send enough funds
+
     // test for purchasing an item that is not for Sale
+    }
+
 
     // shipItem
 
